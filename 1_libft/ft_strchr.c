ccb9c556a78a17d4a84d185994db3568a2a811a4
bugrahankaramollaoglu <beowulf0855@gmail.com>
2023-02-01 15:34:24 +0300

    asd

--- 1_libft/ft_strchr.c
+++ 1_libft/ft_strchr.c
@@ -6,33 +6,25 @@
 /*   By: bkaramol <bkaramol@42istanbul.com.tr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 14:54:51 by bkaramol          #+#    #+#             */
-/*   Updated: 2023/01/30 11:37:23 by bkaramol         ###   ########.fr       */
+/*   Updated: 2023/02/01 15:33:50 by bkaramol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-// s'yi c'den itibaren c dahil sonuna kadar yazdıran fonksiyon
-char	*ft_strchr(const char *s, int c)
-{
-	size_t	i;
-
-	i = 0;
-
-	// s bitene kadar
-	while (s[i])
+char	*ft_strchr(const char *s, int c) {
+	if (s == NULL)
+		return 0;
+	if (c == NULL)
 	{
-		// c'ye gelirsen c dahil stringin kalanını döndür
-		if (s[i] == c)
-			return ((char *)s + i);
-		i++;
+		while (*s)
+			s++;
+		return (s+1);
 	}
+	else {
+		while (*s) {
+			if (*s == c)
+		}
 
-	// 1) c boşsa
-	// 2) s'nin sonuna gelinmişse
-	// kalanını döndür
-	if (!c && s[i] == '\0')
-		return ((char *)s + i);
-	// yoksa null döndür
-	return (NULL);
+	}
 }
