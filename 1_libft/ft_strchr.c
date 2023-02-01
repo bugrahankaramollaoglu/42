4dc309702e414acdd5c92b9449a50017134df850
bugrahankaramollaoglu <beowulf0855@gmail.com>
2023-02-01 15:47:59 +0300

    asd

--- 1_libft/ft_strchr.c
+++ 1_libft/ft_strchr.c
@@ -6,25 +6,31 @@
 /*   By: bkaramol <bkaramol@42istanbul.com.tr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 14:54:51 by bkaramol          #+#    #+#             */
-/*   Updated: 2023/02/01 15:33:50 by bkaramol         ###   ########.fr       */
+/*   Updated: 2023/02/01 15:45:22 by bkaramol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strchr(const char *s, int c) {
-	if (s == NULL)
-		return 0;
-	if (c == NULL)
+char	*ft_strchr(const char *s, int c)
+{
+	if (s[0] == '\0')
+		return (0);
+	if (c == '\0')
 	{
 		while (*s)
 			s++;
-		return (s+1);
+		return ((char *)s);
 	}
-	else {
-		while (*s) {
+	else
+	{
+		while (*s)
+		{
 			if (*s == c)
+				return ((char *)s);
+			s++;
 		}
-
+		return (0);
 	}
 }
+
