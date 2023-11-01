354d020ff8d2bb2b49dfa710007b8395217bdc6f
bugrahankaramollaoglu <beowulf0855@gmail.com>
2023-11-01 16:32:34 +0300

    asd

--- 42_exams/ExamRank_02/level_4/ft_itoa/ft_itoa.c
+++ 42_exams/ExamRank_02/level_4/ft_itoa/ft_itoa.c
@@ -1,60 +1,21 @@
-#include <stdlib.h>
-#include <unistd.h>
+#include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-// basamak sayısı hesaplayan fonksiyon
-int digit(int num)
+char *repeat_str(size_t count, const char *src)
 {
-    int i = 0;
-    if (num == 0)
-        return (1);
-    if (num < 0)
-        i++;
-    while (num)
-    {
-        num /= 10;
-        i++;
-    }
-    return i;
+// allocate a string on the heap
+  char *s = malloc(strlen(src)*count+1);
+  if(!s)
+    return NULL;
+  for(size_t i=0; i<count; ++i) {
+    s = strcpy(s, src);
+    s += strlen(src);
+  }
+  printf("%s\n", s);
+  return s;
 }
 
-char *ft_itoa(int nb)
-{
-    // intin çevrilecegi string
-    char *my_str;
-    // basamak sayısı
-    int dig = digit(nb);
-    // sayının yedegi
-    int nb2 = nb;
-
-    // yer ayırıyoruz basamak sayısı + null kadar
-    my_str = (char *)malloc(sizeof(int) * dig + 1);
-    if (!my_str)
-        return 0;
-    // negatifse başına 0 atıyoruz
-    if (nb < 0)
-    {
-        nb2 = -nb2;
-        my_str[0] = '-';
-    }
-    // %10 metodu sadece son basamagı aldıgı icin sondan baslıyoruz eklemeye
-    // o yüzden sonuna null koyup nuldan 1 sola kaydırıyoruz
-    my_str[dig] = '\0';
-    dig--;
-
-    /* sayı negatifligi (1 ya da 0) dig'den kücük olana kadar döngü yaratıyoruz
-    bunun sebebi şu: eğer sayı negatifse sayı koydugumuz - işaretine kadar eklensin
-    o yüzden 1 <= dig olsun. değilse 0 <= dig olsun.  */
-    while ((nb < 0) <= dig)
-    {
-        my_str[dig] = (nb2 % 10) + 48;
-        nb2 /= 10;
-        dig--;
-    }
-    return (my_str);
-}
-
-int main()
-{
-    printf("%s", ft_itoa(-234));
+int main() {
+  repeat_str(3, "abc");
 }
